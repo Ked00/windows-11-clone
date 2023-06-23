0275f51a4fa457efa173cbb14f7dffea806e9adc
ked00 <treemansquad@gmail.com>
2023-06-23 15:00:52 -0500

    taskbar icon placemeent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,28 @@ export default function Home() {
         quality={100}
         alt="windows 11 desktop image"
       />
-      <div className="bg-red-200 fixed bottom-0 w-full h-11">
-       
-       </div>
+
+      {/* taskbar */}
+      <div className="bg-[#f3f3f3]/[.85] fixed bottom-0 w-full h-[7%] flex justify-between p-3">
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon"/> 
+
+        <div className="flex">
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        </div>
+
+        <div className="flex">
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        <Image src="/../public/cloud.png" width={50} height={50} quality={100} alt="weather icon" className="mx-2"/>
+        </div>
+
+      </div>
     </div>
   );
 }
