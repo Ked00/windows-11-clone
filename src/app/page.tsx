120fa6a8e839d0264626316a1c7c626c55103b22
ked00 <treemansquad@gmail.com>
2023-06-22 22:40:16 -0500

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
+    <div className="static w-full h-3/4">
       <Image
         src="/../public/desktop-image.jpeg"
         fill={true}
         quality={100}
         alt="windows 11 desktop image"
       />
+      <div className="bg-red-200 fixed bottom-0 w-full h-11">
+       
+       </div>
     </div>
   );
 }
