da009a73077ea13547719bb94def63eebbb4d972
ked00 <treemansquad@gmail.com>
2023-06-22 21:58:27 -0500

    tailwind and desktop wallpaper added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <p>hello</p>
-   </div>
-  )
+    <div className="w-full h-full">
+      <Image
+        src="/../public/desktop-image.jpeg"
+        fill={true}
+        quality={100}
+        alt="windows 11 desktop image"
+      />
+    </div>
+  );
 }
