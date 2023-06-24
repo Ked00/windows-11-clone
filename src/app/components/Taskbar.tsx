eeeb0c411a7edc6c59e928539c028982c31fd23b
ked00 <treemansquad@gmail.com>
2023-06-24 11:37:53 -0500

    middle row icons centered in taskbar

--- src/app/components/Taskbar.tsx
+++ src/app/components/Taskbar.tsx
@@ -1,64 +1,69 @@
 "use client";
 import Image from "next/image";
-import {ActionIcon} from "@mantine/core";
-import {IconBrandWindows} from "@tabler/icons-react";
 import {TextInput} from "@mantine/core";
 
 export default function Taskbar() {
   return (
-    <div className="bg-[#f3f3f3]/[.85] fixed bottom-0 w-full h-[7%] flex justify-between">
+    <div className="bg-[#f3f3f3]/[.85] fixed bottom-0 w-full h-[5.5%] flex justify-between">
       {/* left */}
-      <ActionIcon variant="subtle" radius="xs" size="xl">
-        <IconBrandWindows size="xl" />
-      </ActionIcon>
+      <Image
+        src="/../public/weather.png"
+        width={50}
+        height={50}
+        quality={100}
+        alt="weather icon"
+        className="p-2"
+      />
 
       {/* middle */}
-      <div className="flex border-2 border-red-200">
-      <Image
+      <div className="flex justify-center p-2 space-x-5 items-center ml-28">
+        <Image
           src="/../public/home.png"
-          width={50}
-          height={50}
+          width={30}
+          height={30}
           quality={100}
-          alt="weather icon"
-          className=""
+          alt="microsoft menu icon"
         />
 
         <TextInput
           placeholder="Search"
           variant="filled"
           radius="xl"
-          size="sm"
-          sx={{width: "23%"}}
-          className="text-black"
+          size="xs"
         />
+        
         <Image
           src="/../public/edge.png"
-          width={35}
-          height={35}
+          width={30}
+          height={30}
           quality={100}
-          alt="weather icon"
-          className=""
+          alt="microsoft edge icon"
         />
         <Image
           src="/../public/folder.ico"
-          width={50}
-          height={50}
+          width={30}
+          height={30}
           quality={100}
-          alt="weather icon"
-          className=""
+          alt="microsoft folder icon"
         />
         <Image
           src="/../public/store.png"
-          width={50}
-          height={50}
+          width={30}
+          height={30}
           quality={100}
-          alt="weather icon"
-          className=""
+          alt="microsoft store icon"
+        />
+        <Image
+          src="/../public/mail.png"
+          width={30}
+          height={30}
+          quality={100}
+          alt="microsoft mail icon"
         />
       </div>
 
       {/* end */}
-      <div className="flex ">
+      <div className="flex p-3">
         <Image
           src="/../public/cloud.png"
           width={50}
