c73255c2afdbbbe84e0353fa158ad65d10a24a0b
ked00 <treemansquad@gmail.com>
2023-07-14 16:08:12 -0500

    icons and dividers added to header

--- src/app/components/taskbar/fileSystem/FileSystem.tsx
+++ src/app/components/taskbar/fileSystem/FileSystem.tsx
@@ -2,7 +2,7 @@ import IconButton from "../../IconButton";
 import {Modal, Button} from "react-bootstrap";
 import {useIsShowing} from "@/app/hooks/isShowing";
 import Image from "next/image";
-import {ButtonGroup} from "@mui/material";
+import {ButtonGroup, Divider} from "@mui/material";
 
 export default function FileSystem() {
   const isShowing = useIsShowing(false);
@@ -24,23 +24,74 @@ export default function FileSystem() {
         show={isShowing.isShowing}
         size="lg"
       >
-        <Modal.Header className="flex justify-content-between p-2 bg-[#eff4f9]">
-          <div className="flex items-center">
-            <Image width={20} height={20} alt="windows 11 icon" src="/../public/folder.ico" />
-            <p className="ml-1">File Explorer</p>
+        <Modal.Header className="flex flex-col p-2 bg-[#eff4f9]">
+          <div className="flex justify-between w-full">
+            <div className="flex items-center">
+              <Image width={20} height={20} alt="windows 11 icon" src="/../public/folder.ico" />
+              <p className="ml-1">File Explorer</p>
+            </div>
+
+            <ButtonGroup size="small">
+              <Button className="border-none">
+                <Image width={15} height={15} alt="windows 11 icon" src="/../public/minimize.png" />
+              </Button>
+              <Button className="border-none">
+                <Image
+                  width={15}
+                  height={15}
+                  alt="windows 11 icon"
+                  src="/../public/restoreDown.png"
+                />
+              </Button>
+              <Button className="border-none">
+                <Image width={15} height={15} alt="windows 11 icon" src="/../public/close.png" />
+              </Button>
+            </ButtonGroup>
           </div>
 
-          <ButtonGroup size="small" variant="text">
-            <Button>
-              <Image width={20} height={20} alt="windows 11 icon" src="/../public/folder.ico" />
+          <div className="w-full flex mt-4">
+            <Button className="flex items-center border-none text-black">
+              <Image width={19} height={19} alt="windows 11 icon" src="/../public/new.png" />
+              <span className="ml-1">New</span>
             </Button>
-            <Button>
-              <Image width={20} height={20} alt="windows 11 icon" src="/../public/folder.ico" />
-            </Button>
-            <Button>
-              <Image width={20} height={20} alt="windows 11 icon" src="/../public/folder.ico" />
-            </Button>
-          </ButtonGroup>
+
+            <div>
+              <Divider orientation="vertical" variant="fullWidth" className="bg-black mx-1" />
+            </div>
+
+            <ButtonGroup variant="text">
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/cut.png" />
+              </Button>
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/copy.png" />
+              </Button>
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/paste.png" />
+              </Button>
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/rename.png" />
+              </Button>
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/share.png" />
+              </Button>
+            </ButtonGroup>
+
+            <div>
+              <Divider orientation="vertical" variant="fullWidth" className="bg-black mx-1" />
+            </div>
+
+            <ButtonGroup variant="text">
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/sort.png" />
+                <span className="ml-1">Sort</span>
+              </Button>
+              <Button className="flex items-center border-none text-black">
+                <Image width={19} height={19} alt="windows 11 icon" src="/../public/view.png" />
+                <span className="ml-1">View</span>
+              </Button>
+            </ButtonGroup>
+          </div>
         </Modal.Header>
         <Modal.Body></Modal.Body>
       </Modal>
